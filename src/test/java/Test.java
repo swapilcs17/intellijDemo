87ebbcc822bf26499269d32232b86aed5dc5238d
swapnilcs <swapnil.salunke47@gmail.com>
2020-07-30 12:55:13 +0530

    Revert "Add something in java file"
    This reverts commit 614c9690916a6c0038a1d95ac239e26efbccd136.

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Real Madrid");
+        System.out.println("Real Madrid is the best football club in the world");
     }
 }
